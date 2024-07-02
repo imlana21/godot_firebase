7bf527036c5bba7d15058ec52e233ede86ab216d
Ihwan M <ihwan.cfc@gmail.com>
2024-07-02 20:31:16 +0700

    fix scratch features

--- project/main.gd
+++ project/main.gd
@@ -1,27 +1,55 @@
 extends Control
 
-@onready var scratch_screen = $ScratchScreen
+@onready var scratch_screen := $ScratchScreen
 
-var registered_number: Array = [10, 11, 20]
+var mouse_pressed: bool = false
+var curr_line: Line2D
+var registered_number: Array = []
+var brush_size = 10
+var brush_color = Color(1, 1, 1, 0)
+var image: Image
+var texture: ImageTexture
+
 
 func _on_button_pressed():	
 	Firebase.Auth.logout()
 	get_tree().change_scene_to_file("res://project/auth.tscn")
 
+func _input(event):
+	if event is InputEventMouseButton:
+		mouse_pressed = event.pressed
+		# if mouse_pressed:
+		# 	curr_line = Line2D.new()
+		# 	curr_line.default_color = Color.WHITE
+		# 	$Line.add_child(curr_line)
+	if event is InputEventMouseMotion && mouse_pressed:
+		# curr_line.add_point(event.position)
+		draw_on_texture(event.position)
+	
+func draw_on_texture(pos):
+	if $PenCanvas.texture:
+		# var image = $PenCanvas.texture.get_data()
+		# image.lock()
+		for i in range(-brush_size, brush_size):
+			for j in range(-brush_size, brush_size):
+				var dist = sqrt(i * i + j * j)
+				if dist < brush_size:
+					var pixel_pos = Vector2i(pos.x + i, pos.y + j)
+					if pixel_pos.x >= 0 and pixel_pos.x < image.get_width() and pixel_pos.y >= 0 and pixel_pos.y < image.get_height():
+						image.set_pixelv(pixel_pos, brush_color)
+		# image.unlock()
+		texture = ImageTexture.create_from_image(image)
+		$PenCanvas.texture = texture
+		# $PenCanvas.texture.create_from_image(image)
+
 func _ready():
+	image = Image.create(int(get_viewport_rect().size.x), int(get_viewport_rect().size.y), false, Image.FORMAT_RGBA8)
+	image.fill(Color('000000'))
+	texture = ImageTexture.create_from_image(image)
+	$PenCanvas.texture = texture
 	$AnimationPlayer.play('default')
 	for screen in scratch_screen.get_children():
-		screen.connect('screen_clicked', _on_scratch_screen_clicked)
-
-func _on_scratch_screen_clicked(screen: ColorRect):
-	if !screen.status:
-		var label = load("res://project/properties/screen_label.tscn").instantiate()
-		label.set_anchors_preset(Control.PRESET_CENTER_BOTTOM)
-		label.scale = Vector2(0.7, 0.7)
-		label.set_label_text(get_unique_randomize())
-		screen.add_child(label)
-		screen.self_modulate = 'ffffff00'
-		screen.status = true
+		screen.set_label_text(get_unique_randomize())
 
 func get_unique_randomize():
 	var rand_number = randi_range(1, 300)
---
+++ project/scratch_panel.gd
@@ -0,0 +1,4 @@
+extends Panel
+
+func set_label_text(text: String):
+	$ScreenLabel/Label.text = text
