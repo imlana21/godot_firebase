70f77bb517399dc058f53ce7925ef0e7058481b2
Ihwan M <ihwan.cfc@gmail.com>
2024-07-21 17:43:17 +0700

    add number to wheel

--- project/popup/wheel/wheel.gd
+++ project/popup/wheel/wheel.gd
@@ -1,9 +1,9 @@
 extends Control
 
-var rotation_speed: float = 300.0
+var rotation_speed: float = 360.0
 var is_spinning: bool = false
 var target_angle: float = 0.0  # Sudut target untuk berhenti
-var stop_angle_threshold = 2.0  # Ambang batas untuk berhenti di target
+var stop_angle_threshold = 3.0  # Ambang batas untuk berhenti di target
 var segment_count = 12  # Jumlah segmen dalam roda
 var one_round: float = 360.0
 var wheel_rotation: int = 0
@@ -25,6 +25,7 @@ func _process(delta):
 	if is_spinning:
 		spin_wheel(delta)
 		if abs($SpriteList/Content.rotation_degrees - target_angle) < stop_angle_threshold:
+			is_spinning = false
 			print("Gacha result: ", get_gacha_result())
 		$SpinBtn.disabled = true
 	else:
@@ -32,18 +33,15 @@ func _process(delta):
 
 func spin_wheel(delta):
 	$SpriteList/Content.rotation_degrees += rotation_speed * delta
-	if int($SpriteList/Content.rotation_degrees) % int(one_round) == 0:
-		wheel_rotation += 1
 		
 	if rotation_speed > 0:
-		if wheel_rotation > 2:
-			rotation_speed -= 50 * delta
+		rotation_speed -= 30 * delta
 	else:
 		is_spinning = false
 
 func get_random_gacha_angle():
 	var random_value = randi() % segment_count
-	return random_value * (360.0 / segment_count)
+	return random_value * (one_round * 2 / segment_count)
 
 func get_gacha_result():
 	var index = int(wrapf($SpriteList/Content.rotation_degrees, 0, one_round) / (one_round / segment_count))
@@ -52,7 +50,7 @@ func get_gacha_result():
 func reset():
 	rotation_speed = 300.0
 	target_angle = 0.0  # Sudut target untuk berhenti
-	stop_angle_threshold = 2.0 
+	stop_angle_threshold = 3.0 
 	wheel_rotation = 0
 
 func show_popup() -> void:
